095d1106b03d7341e38f555a8d9f637c4de93275
FocusPunch <cjbellando@gmail.com>
2022-01-14 00:32:04 -0600

    Initial commit

---
+++ media/lua/client/timetocook.lua
@@ -0,0 +1,19 @@
+require 'ISUI/ISInventoryPane'
+
+TimeToCookMod = {}
+
+TimeToCookMod.ISInventoryPaneDrawItemDetails = ISInventoryPane.drawItemDetails
+
+function ISInventoryPane:drawItemDetails(item, y, xoff, yoff, red)
+    TimeToCookMod.ISInventoryPaneDrawItemDetails(self, item, y, xoff, yoff, red)
+    if instanceof(item, "Food") then
+        local top = self.headerHgt + y * self.itemHgt + yoff
+        local fgBar = {r=0.0, g=0.6, b=0.0, a=0.7}
+	    local fgText = {r=0.6, g=0.8, b=0.5, a=0.6}
+        if(item:isCookable() and item:getCookingTime() < item:getMinutesToCook()) then
+            self:drawText("Minutes to Cook: " .. (item:getMinutesToCook()/2), 40 + 30 + xoff + 200, top + (self.itemHgt - self.fontHgt) / 2, fgText.a, fgText.r, fgText.g, fgText.b, self.font);
+
+        end
+    end
+end
+
